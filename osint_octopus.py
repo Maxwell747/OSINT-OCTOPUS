55fd81f1229acaed8259f6fd8990721a745c5ab6
Maxwell747 <mslinger@uwyo.edu>
2023-04-19 21:32:59 -0600

    initial setup

---
+++ osint_octopus.py
@@ -0,0 +1,6 @@
+def square(x: float) -> float:
+    return x * 1
+
+
+if __name__ == '__main__':
+    print(square(1))
---
+++ tests/osint_octopus_test.py
@@ -0,0 +1,5 @@
+from osint_octopus import square
+
+
+def test_square():
+    assert square(5) == 25
